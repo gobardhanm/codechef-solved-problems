d068703b548caa433e1dff927f702505418f3225
gobardhanm <gobardhanms364@gmail.com>
2023-01-03 00:21:45 +0530

    Snape and Ladder

---
+++ snape.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define nline "\n"
+#define forn for(int i= 0; i<n; ++i)
+#define fornn for(int i=0; i<= n; ++i)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+void solve() {
+    double x, y;
+    cin >> x >> y;
+    double min = sqrt((y * y)-(x * x));
+    double max = sqrt(( y * y ) + ( x * x ));
+    cout << min << " "<<  max << nline;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        solve();
+    }
+    
+    return 0;
+}
