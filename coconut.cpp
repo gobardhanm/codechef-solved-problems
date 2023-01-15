e096821ecc27086e72ddeee3c7aec3d70229679a
gobardhanm <gobardhanms364@gmail.com>
2023-01-15 15:36:16 +0530

    Summer Heat

---
+++ coconut.cpp
@@ -0,0 +1,38 @@
+//Gobardhan Meher
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define nline "\n"
+#define fori for(int i= 0; i<n; ++i)
+#define forj for(int j = 0; j< n; ++j)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+//Problem: Summer Heat
+
+void solve() {
+    int a, b , x , y;
+    cin >> a >> b >> x >> y;
+    cout << (x/a) + (y/b) << nline;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        solve();
+    }
+    
+    return 0;
+}
