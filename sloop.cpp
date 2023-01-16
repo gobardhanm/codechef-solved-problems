0c9282eaa840e82f0e6bad153d7a160a8e4870d5
gobardhanm <gobardhanms364@gmail.com>
2023-01-16 15:10:30 +0530

    Coldplay

---
+++ sloop.cpp
@@ -0,0 +1,38 @@
+//Gobardhan Meher
+
+#include<bits/stdc++.h>
+using namespace std;
+
+
+#define ll long long
+#define nline "\n"
+#define fori for(int i= 0; i<n; ++i)
+#define forj for(int j = 0; j< n; ++j)
+#define pb push_back
+#define pp pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+//Problem: Coldplay
+
+void solve() {
+    int dist, dur;
+    cin >> dist >> dur;
+    cout << dist/dur << nline;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+
+    int tc = 1;
+    cin >> tc;
+    for(int i = 1; i <= tc; i++) {
+        solve();
+    }
+    
+    return 0;
+}
