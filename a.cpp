f4cf7b599906ad689ce2fc098fd25a15b719beee
gobardhanm <gobardhanms364@gmail.com>
2023-03-04 12:59:27 +0530

    Newly Added

---
+++ a.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// 
+
+void solve() {
+    cout << "Ello Mate !" << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int tc = 1;
+    cin >> tc;
+    while(tc--)
+        solve();
+
+    return 0;
+}
+
